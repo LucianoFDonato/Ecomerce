b61fc94d39076bad581d494aaea38200f3a8c180
Luciano <lucianofdonato@gmail.com>
2022-12-21 10:42:32 -0300

    1

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ let userInput = document.querySelector('.input__number');
 
 let userInputNumber = 0;
 
-masBtn.addEventListener('click', ()=> {
+masBtn.addEventListener('click', () => {
     userInputNumber++;
     userInput.value = userInputNumber;
     console.log('userInputNumber');
 })
-minusBtn.addEventListener('click', ()=> {
+minusBtn.addEventListener('click', () => {
     userInputNumber--;
-    if(userInputNumber <= 0){
+    if (userInputNumber <= 0) {
         userInputNumber = 0;
     }
     userInput.value = userInputNumber;
@@ -24,11 +24,60 @@ minusBtn.addEventListener('click', ()=> {
 
 const addToCartBtn = document.querySelector('.details__button');
 let cartNotification = document.querySelector('.header__cart--notification');
+let lastValue = parseInt(cartNotification.innerText);
 
-addToCartBtn.addEventListener('click', ()=>{
+addToCartBtn.addEventListener('click', () => {
+    lastValue = lastValue + userInputNumber;
 
-    let lastValue = cartNotification.innerText;
-
-    cartNotification.innerText = userInputNumber;
+    cartNotification.innerText = lastValue;
     cartNotification.style.display = "block";
+    drawProductInModal();
+    priceModal.innerHTML = `$125.00 x${lastValue} <span>$${lastValue*125}.00</span>`;
 })
+
+// Mostrando modal del carrito.
+
+const cartIconBtn = document.querySelector('.header__cart');
+const cartModal = document.querySelector('.cart-modal');
+let priceModal = document.querySelector('.cart-modal__price');
+const productConteiner = document.querySelector('.cart-modal__checkout-container');
+
+
+cartIconBtn.addEventListener('click', () => {
+    cartModal.classList.toggle('show');
+
+    if (lastValue == 0) {
+        drawProductInModal();
+    }
+})
+
+// Borrar contenido del carrito.
+function deleteProduct(){
+    const deleteProductBtn = document.querySelector('.cart-modal__delete');
+
+deleteProductBtn.addEventListener('click', () => {
+    productConteiner.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
+    lastValue = 0;
+    cartNotification.innerText = lastValue;
+})
+
+}
+
+
+// Funciones.
+
+function drawProductInModal() {
+    productConteiner.innerHTML = `
+        <div class="cart-modal__checkout-container">
+                    <div class="cart-modal__details-container">
+                        <img class="cart-modal__image" src="imagenes/gafas.png" alt="">
+                        <div>
+                            <p class="cart-modal__product">Autumn limited edition</p>
+                            <p class="cart-modal__price">$125.00 x0 <span>$.00</span></p>
+                        </div>
+                        <img class="cart-modal__delete" src="imagenes/delete.png" alt="delete">
+    
+                    </div>
+                    <button class="cart-modal__checkout">Checkout</button>`;
+                    deleteProduct()
+}
