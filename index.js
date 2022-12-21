2d5dd4730743b7425b3038141f2978fbeb37353c
Luciano <lucianofdonato@gmail.com>
2022-12-21 11:36:39 -0300

    2

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ addToCartBtn.addEventListener('click', () => {
     cartNotification.innerText = lastValue;
     cartNotification.style.display = "block";
     drawProductInModal();
-    priceModal.innerHTML = `$125.00 x${lastValue} <span>$${lastValue*125}.00</span>`;
 })
 
 // Mostrando modal del carrito.
@@ -52,16 +51,36 @@ cartIconBtn.addEventListener('click', () => {
 })
 
 // Borrar contenido del carrito.
-function deleteProduct(){
+function deleteProduct() {
     const deleteProductBtn = document.querySelector('.cart-modal__delete');
 
-deleteProductBtn.addEventListener('click', () => {
-    productConteiner.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
-    lastValue = 0;
-    cartNotification.innerText = lastValue;
-})
+    deleteProductBtn.addEventListener('click', () => {
+        productConteiner.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
+        lastValue = 0;
+        cartNotification.innerText = lastValue;
+    })
 
 }
+
+// Cambiar imagenes al tocar btn flecha.
+const imageContainer = document.querySelector('.gallery__image-container');
+const previousBtn = document.querySelector('.gallery__previous');
+const nextBtn = document.querySelector('.gallery__next');
+let imgIndex = 1;
+const imageUrls = [
+    '../imagenes/gafas1.jpg',
+    '../imagenes/gafas2.jpg',
+    '../imagenes/gafas3.jpg',
+    '../imagenes/gafas4.jpg',
+]
+
+nextBtn.addEventListener('click', ()=>{
+    changeNextImage(imageContainer);
+})
+previousBtn.addEventListener('click', ()=>{
+    changePreviousImage(imageContainer);
+})
+
 
 
 // Funciones.
@@ -79,5 +98,22 @@ function drawProductInModal() {
     
                     </div>
                     <button class="cart-modal__checkout">Checkout</button>`;
-                    deleteProduct()
+    deleteProduct()
+    let priceModal = document.querySelector('.cart-modal__price');
+    priceModal.innerHTML = `$125.00 x${lastValue} <span>$${lastValue*125}.00</span>`;
+}
+
+function changeNextImage(imgContainer){
+    if(imgIndex == 4){
+        imgIndex = 0;
+    }
+    imgIndex++;
+    imgContainer.style.backgroundImage = `url('../imagenes/gafas${imgIndex}.jpg')`
+}
+function changePreviousImage(imgContainer){
+    if(imgIndex == 0){
+        imgIndex = 4;
+    }
+    imgIndex--;
+    imgContainer.style.backgroundImage = `url('../imagenes/gafas${imgIndex}.jpg')`
 }
